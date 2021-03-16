78edb2cf3014e930d72f9b586b1ad2ee10334c1a
popesz2000 <popesz2000@gmail.com>
2021-03-16 15:45:03 +0100

    Implement timer

--- game.js
+++ game.js
@@ -4,4 +4,19 @@ function initGame() {
 
     // Your game can start here, but define separate functions, don't write everything in here :)
 
+    startTimer();
+}
+
+function startTimer(){
+    let sec = 0, min = 0;
+    setInterval(function(){
+        let timer = document.querySelector(".timer");
+        timer.innerHTML = "Timer:" +min+ " Minutes " +sec+ " Seconds";
+        sec++;
+
+        if(sec == 60){
+            min++;
+            sec=0;
+        }
+    },1000);
 }
