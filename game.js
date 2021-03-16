834586fa88de1125a0da7ef61788038d75b7ee74
Taowathar <taowathar@gmail.com>
2021-03-16 16:48:11 +0100

    Hide the unmatched pictures

--- game.js
+++ game.js
@@ -1,10 +1,31 @@
 initGame();
+let moves = 0;
 
 function initGame() {
     shuffleCards()
     initLeftClick()
-    // Your game can start here, but define separate functions, don't write everything in here :)
+    startTimer();
+}
 
+
+function startTimer(){
+    let sec = 0, min = 0;
+    setInterval(function(){
+        let timer = document.querySelector(".timer");
+        timer.innerHTML = "Timer:" +min+ " Minutes " +sec+ " Seconds";
+        sec++;
+
+        if(sec == 60){
+            min++;
+            sec=0;
+        }
+    },1000);
+}
+
+function moveCounter() {
+    let move_count = document.querySelector(".move_count")
+    moves++;
+    move_count.innerHTML = moves + " Moves";
 }
 
 
@@ -17,9 +38,29 @@ function shuffleCards() {
 
 function initLeftClick() {
     let cards = document.querySelectorAll(".card");
+    let counter = 0;
     for (let card of cards) {
         card.onclick = function(event) {
-            event.currentTarget.children[0].classList.remove('hidden')
+            moveCounter()
+            event.currentTarget.children[0].classList.remove('hidden');
+            counter++;
+            if (counter === 1) {
+                window.firstPick = event.currentTarget.children[0];
+            }
+
+            if (counter === 2) {
+                window.secondPick = event.currentTarget.children[0]
+                if (event.currentTarget === firstPick) {}
+                else {
+                    setTimeout(function() {
+                        secondPick.classList.add('hidden')
+                        firstPick.classList.add('hidden')
+                    }, 3000)
+
+                }
+                counter = 0
+            }
         }
     }
+
 }
