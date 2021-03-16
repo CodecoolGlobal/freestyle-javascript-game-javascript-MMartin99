56f7ec5f7a2560b63c95f968acc7ae8b2ad78786
Taowathar <taowathar@gmail.com>
2021-03-16 15:32:43 +0100

    Implement shuffleCards function

--- game.js
+++ game.js
@@ -1,7 +1,14 @@
 initGame();
 
 function initGame() {
-
+    shuffleCards()
     // Your game can start here, but define separate functions, don't write everything in here :)
 
+}
+
+
+function shuffleCards() {
+    let board = document.querySelector(".main-board");
+    for (let i = board.children.length; i >= 0; i--) {
+        board.appendChild(board.children[Math.random() * i | 0]);}
 }
