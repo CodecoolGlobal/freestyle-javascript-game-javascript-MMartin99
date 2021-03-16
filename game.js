a546ff00c72a187cfca8d6bf5a04e7cf656569b1
Taowathar <taowathar@gmail.com>
2021-03-16 16:08:21 +0100

    Show the hidden card on leftclick

--- game.js
+++ game.js
@@ -2,6 +2,7 @@ initGame();
 
 function initGame() {
     shuffleCards()
+    initLeftClick()
     // Your game can start here, but define separate functions, don't write everything in here :)
 
 }
@@ -10,5 +11,15 @@ function initGame() {
 function shuffleCards() {
     let board = document.querySelector(".main-board");
     for (let i = board.children.length; i >= 0; i--) {
-        board.appendChild(board.children[Math.random() * i | 0]);}
+        board.appendChild(board.children[Math.random() * i | 0]);};
+}
+
+
+function initLeftClick() {
+    let cards = document.querySelectorAll(".card");
+    for (let card of cards) {
+        card.onclick = function(event) {
+            event.currentTarget.children[0].classList.remove('hidden')
+        }
+    }
 }
